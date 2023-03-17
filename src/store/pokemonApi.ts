14303819f4c8314183c5766adbcb8ed35c66acfa
Victor Akintunde <akintoluvic@gmail.com>
2023-03-17 21:05:06 +0100

    pokemon slice api

---
+++ src/store/pokemonApi.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+import { Pokemon } from "@/types";
+
+export const pokemonApi = createApi({
+    reducerPath: "pokemonApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/api/"}),
+    tagTypes: ['pokemon'],
+    endpoints: (builder) => ({
+        search: builder.query<Pokemon[], string>({
+            query: q => `search?name=${q}`,
+            providesTags: (result, error, search) => [{ type: "pokemon", search}]
+        })
+    })
+})
+ 
