2fe4fee88cff3bb5456b38a6e4ef19c60ba88b23
Victor Akintunde <akintoluvic@gmail.com>
2023-03-17 20:38:47 +0100

    pokemon table

--- src/app/components/SearchInput.tsx
+++ src/app/components/SearchInput.tsx
@@ -5,13 +5,24 @@ import type { TypedUseSelectorHook } from "react-redux";
 import { RootState, AppDispatch } from "@/store";
 import { setSearch } from "@/store/searchSlice";
 
+import PokemonTable from "./PokemonTable";
+
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const SearchInput = () => {
+  const dispatch = useAppDispatch();
+  const search = useAppSelector((state) => state.search.search);
+  const startupPokemon = useAppSelector((state) => state.search.startupPokemon);
+
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => dispatch(setSearch(e.target.value))}
+      />
+      <PokemonTable pokemons={startupPokemon} />
     </div>
   );
 };
